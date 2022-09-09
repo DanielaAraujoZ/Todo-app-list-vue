965b030cd70a2fb11f8472cecaf54a130bdd7771
Daniela Araujo <daniela@mimegafono.com>
2022-09-08 20:35:25 -0500

    Initial layout and functionalitiesn

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="h-screen flex justify-center items-center">
+    <div class="h-4/5 bg-white w-2/6 rounded-2xl relative pt-8">
+      <TodoInputNew @set-value="addNewTask"/>
+      <TodoItem :listTasksTodo="listTasksTodo" @delete-task="deleteTask"/>
+    </div>
+  </main>
+</template>
+
+<script>
+  import TodoItem from './components/TodoItem.vue';
+  import TodoInputNew from './components/TodoInputNew.vue';
+
+  export default{
+    components:{
+      TodoItem, TodoInputNew
+    },
+    data(){
+      return{
+        listTasksTodo: []
+      }
+    },
+    methods:{
+      addNewTask(taskItem){
+        this.listTasksTodo.push(taskItem);
+      },
+      deleteTask(index){
+        this.listTasksTodo.splice(index,1);
+      }
+    }
+  }
+  
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+  html{
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
---
+++ src/components/TodoInputNew.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-4/5 mx-auto mt-3 h-12 flex items-center gap-4">
+        <input type="text" name="" id="" class="h-full w-9/12 px-4 border rounded-xl focus:outline-none focus:ring-violet-500 focus:ring-1" v-model="taskItem">
+        <button type="button" class="h-full bg-violet-400 text-white w-3/12 rounded-xl" @click="setValue"> Add Task </button>
+    </div>
+</template>
+
+<script>
+    export default{
+        emits:['set-value'],
+        data(){
+            return{
+                taskItem: ''
+            }
+        },
+        computed:{
+            setValue(){
+                this.$emit(
+                    'set-value',
+                    this.taskItem
+                );
+                this.taskItem = ''
+            }
+        }
+    }
+
+</script>
+
+<style >
+
+</style>
